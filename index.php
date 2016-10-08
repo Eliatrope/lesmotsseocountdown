3f5d8bf8472fd583d8f58737d99e4a0311166978
Eliatrope <hugo.ballabriga@laposte.net>
2016-10-08 19:29:42 +0200

    Anim cloud

--- index.php
+++ index.php
@@ -11,9 +11,19 @@
     <section class="main_container">
       <header class="main_header">
         <img class="main_logo" src="assets/images/les_mots_seo.png" alt="Les Mots SEO" title="Les Mots SEO" />
+        <h1 class="main_title">
+          arrivent très bientôt!
+          <img src="assets/images/one_drive.png" alt="Soon" title="Soon"/>
+        </h1>
       </header>
+      <div id="myCounter"></div>
     </section>
     <script src="assets/js/jquery-2.2.0.min.js"></script>
+    <script src="assets/js/jquery.mb-comingsoon.js"></script>
     <script src="assets/js/main.js"></script>
+    <script>
+      $('#myCounter').mbComingsoon(new Date(2016, 5, 12, 9)) //Expires
+      $('#myCounter').mbComingSoon('start')       // start counter
+    </script>
   </body>
 </html>
