843cf9cb82e162a40a7f563c901cee32ad718e1e
Eliatrope <hugo.ballabriga@laposte.net>
2016-10-08 18:04:44 +0200

    Fichiers de base
    Création

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="FR-fr" dir="rtl">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Les mots SEO">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link href="/assets/images/favicon/logopufrweb.ico" rel='icon' type='img/ico' />
+  </head>
