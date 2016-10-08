40a1cb9af53309e6845030ddb0499b0a90755516
Julien <ballanger.julien@gmail.com>
2016-10-08 20:06:24 +0200

    assets

--- admin/index.php
+++ admin/index.php
@@ -12,22 +12,27 @@
         exit(0);
     }
 
-    if (isset($_POST['username']) && isset($_POST['password']) || isset($_SESSION['username']) && isset($_SESSION['password']))
+    if (!isset($_SESSION['username']) && !(isset($_SESSION['password'])))
     {
-        $query = $dbh->prepare("SELECT username, password FROM users WHERE username = :username AND password = :password");
-        $query->bindParam(":username", $_POST['username'], PDO::PARAM_STR);
-        $query->bindParam(":password", $_POST['password'], PDO::PARAM_STR);
-        $query->execute();
-
-        if ($query->rowCount() == 1)
+        if (isset($_POST['username']) && isset($_POST['password']))
         {
-            $_SESSION['username'] = $_POST['username'];
-            $_SESSION['password'] = $_POST['password'];
-            require('dashboard.php');
+            $query = $dbh->prepare("SELECT username, password FROM users WHERE username = :username AND password = :password");
+            $query->bindParam(":username", $_POST['username'], PDO::PARAM_STR);
+            $query->bindParam(":password", $_POST['password'], PDO::PARAM_STR);
+            $query->execute();
+
+            if ($query->rowCount() == 1)
+            {
+                $_SESSION['username'] = $_POST['username'];
+                $_SESSION['password'] = $_POST['password'];
+                header('Location:dashboard.php');
+            }
+            else
+                header('Location:index.php');
         }
         else
-            header('Location:index.php');
+            require('login.html');
     }
     else
-        require('login.html');
+        header('Location:dashboard.php');
 ?>
